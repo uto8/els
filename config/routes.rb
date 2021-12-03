f08c2e5bf6dc7110797da9642d81bd9ed6ad5498
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2021-12-03 22:18:07 +0900

    create signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'user/new'
   root "pages#home"
   get '/about', to:'pages#about'
   get '/contact', to:'pages#contact'
+  resources :users
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
