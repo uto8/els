00f2638c8ff758777c29901736312d82b5a357cd
uto8 <rb7e8bfa72xe6ee05se@softbank.ne.jp>
2021-12-06 21:04:40 +0900

    created login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
   get 'users/new'
   get 'user/new'
   root "pages#home"
@@ -6,5 +8,8 @@ Rails.application.routes.draw do
   get '/contact', to:'pages#contact'
   resources :users
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  resources :sessions, only: :create
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
